b5a1372e466449518cb8f6348c286d27608426b2
ASIF <asifm@webelight.co.in>
2023-01-16 16:30:56 +0530

    Done

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -1,25 +1,57 @@
 let turn = "X"
+let gameover = false
 
-const changeTurn = () =>{
-
-    return turn === "X"?"0":"X"
+const changeTurn = () => {
+    return turn === "X" ? "0" : "X"
 }
 
-const checkWin = () =>{
-
+const checkWin = () => {
+    let boxtexts = document.getElementsByClassName('boxtext');
+    let wins = [
+        [0, 1, 2, 5, 5, 0],
+        [3, 4, 5, 5, 15, 0],
+        [6, 7, 8, 5, 25, 0],
+        [0, 3, 6, -5, 15, 90],
+        [1, 4, 7, 5, 15, 90],
+        [2, 5, 8, 15, 15, 90],
+        [0, 4, 8, 5, 15, 45],
+        [2, 4, 6, 5, 15, 135],
+    ]
+    wins.forEach(e => {
+        if ((boxtexts[e[0]].innerText === boxtexts[e[1]].innerText) && (boxtexts[e[2]].innerText === boxtexts[e[1]].innerText) && (boxtexts[e[0]].innerText !== "")) {
+            document.querySelector('.info').innerText = boxtexts[e[0]].innerText + " Won"
+            gameover = true
+            document.querySelector(".line").style.width = "20vw"
+            document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`
+        }
+    })
 }
 
 let boxes = document.getElementsByClassName("box")
-Array.from(boxes).forEach(element =>{
-    let boxtext = document.querySelector('.boxtext')
-    element.addEventListener('click',() => {
+Array.from(boxes).forEach(element => {
+    let boxtext = element.querySelector('.boxtext')
+    element.addEventListener('click', () => {
 
         if (boxtext.innerText === '') {
             boxtext.innerText = turn
-            changeTurn()
+            turn = changeTurn()
             checkWin()
-            document.getElementsByClassName(turn)[0].innerText = "Turn For " + turn     
+            if (!gameover) {
+                document.getElementsByClassName("info")[0].innerText = "Turn For " + turn
+            }
         }
-
     })
+})
+
+reset.addEventListener('click', () => {
+    let boxtexts = document.querySelectorAll('.boxtext')
+    Array.from(boxtexts).forEach(element => {
+        element.innerText = ""
+    });
+
+    turn = "X"
+    gameover = false
+    document.querySelector(".line").style.width = "0vw"
+    document.getElementsByClassName("info")[0].innerText = "Turn For " + turn
+    
 })
